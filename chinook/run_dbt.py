c89584fe10bb267703d1687e8ecec9e61eccd445
sch-paulo <pschmitzjr@gmail.com>
2025-06-07 17:32:46 -0400

    establishing connection to the database

---
+++ chinook/run_dbt.py
@@ -0,0 +1,12 @@
+'''
+File to detect the environment variables from .env when running "dbt debug"
+'''
+import os
+from dotenv import load_dotenv
+import subprocess
+
+# Load environment variables from .env
+load_dotenv(dotenv_path="../.env")
+
+# Run dbt debug (or any dbt command)
+subprocess.run(["dbt", "debug"])
